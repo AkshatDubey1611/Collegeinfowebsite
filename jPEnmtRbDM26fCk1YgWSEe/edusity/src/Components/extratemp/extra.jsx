460cfe9ca61642110235b195ad06689810125711
AkshatDubey1611 <f20230954@pilani.bits-pilani.ac.in>
2025-06-26 17:42:47 +0530

    Update extra.jsx

--- jPEnmtRbDM26fCk1YgWSEe/edusity/src/Components/extratemp/extra.jsx
+++ jPEnmtRbDM26fCk1YgWSEe/edusity/src/Components/extratemp/extra.jsx
@@ -4,14 +4,48 @@ import "./extra.css";
 const Extra = () => {
   return (
     <div className="extra-container">
+      {/* Seat Matrix Box */}
       <div className="extra-box">
-        <h3>Seat Matrix</h3>
-        <p className="know-more">Know more</p>
+        <h3>SEAT MATRIX</h3>
+        <table className="seat-matrix-table">
+          <thead>
+            <tr>
+              <th>Branch</th>
+              <th>Total Seats</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr><td>Computer Science and Engineering</td><td>104</td></tr>
+            <tr><td>Electrical Engineering</td><td>120</td></tr>
+            <tr><td>Mechanical Engineering</td><td>90</td></tr>
+            <tr><td>Chemical Engineering</td><td>85</td></tr>
+            <tr><td>Civil Engineering</td><td>95</td></tr>
+            <tr><td>Engineering Physics</td><td>50</td></tr>
+          </tbody>
+        </table>
+        <a className="know-more" href="#full-seat-matrix">Know more</a>
       </div>
 
+      {/* Cutoffs Box */}
       <div className="extra-box">
-        <h3>Cutoffs</h3>
-        <p className="know-more">Know more</p>
+        <h3>CUTOFFS</h3>
+        <table className="seat-matrix-table">
+          <thead>
+            <tr>
+              <th>Branch</th>
+              <th>Closing Rank</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr><td>Computer Science and Engineering</td><td>117</td></tr>
+            <tr><td>Electrical Engineering</td><td>387</td></tr>
+            <tr><td>Mechanical Engineering</td><td>890</td></tr>
+            <tr><td>Chemical Engineering</td><td>1500</td></tr>
+            <tr><td>Civil Engineering</td><td>2100</td></tr>
+            <tr><td>Engineering Physics</td><td>1420</td></tr>
+          </tbody>
+        </table>
+        <a className="know-more" href="#full-cutoff">Know more</a>
       </div>
     </div>
   );
